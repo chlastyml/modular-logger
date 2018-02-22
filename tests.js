8af0f78b31d937379d57bf19aa8cb063c1e261a4
Tomáš Kacálek <tomas.kacalek@hellit.cz>
2018-02-22 17:22:51 +0100

    edit tests

--- tests.js
+++ tests.js
@@ -3,15 +3,18 @@ const main = require("./index");
 const helper = require("./helper");
 const enums = require("./enums");
 
-var namespace = helper.colorText("O365", [main.GetFgColor('red')]);
-
-var logger = main.GetLogger(namespace);
+var logger = main.CreateLogger("O365", [main.GetFgColor('red')]);
+var logger2 = main.GetLogger("O365");
 
 console.log("**** START TESTS ****");
+logAll(logger);
+logAll(logger2);
 
-logger.log("Testovaci log");
-logger.logTrace("TRACE log");
-logger.logDebug("DEBUG log");
-logger.logInfo("INFO log");
-logger.logWarning("WARNING log");
-logger.logError("ERROR log");
+function logAll(logger){
+    logger.log("Testovaci log");
+    logger.logTrace("TRACE log");
+    logger.logDebug("DEBUG log");
+    logger.logInfo("INFO log");
+    logger.logWarning("WARNING log");
+    logger.logError("ERROR log");
+}
