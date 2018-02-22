e14a07d0589cd9c6c4ef964304a296b0c327c47f
Tomáš Kacálek <tomas.kacalek@hellit.cz>
2018-02-22 17:06:27 +0100

    add intern logger + public getter on colors and special modificator

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const enums = require("./enums");
 const options = require("./options");
 const loggerModule = require("./logger");
+const helper = require('./helper');
+
+exports.Logger = internLogger;
+var internLogger = new loggerModule.Logger(helper.colorText("Logger", [enums.specials.reverse]), enums.logLevel.info);
 
 var loggers = [];
 
@@ -8,9 +12,34 @@ exports.GetLogger = (namespace = options.defaultNamespace, color = 'white', minL
     var resultLogger = loggers[namespace.toLowerCase()];
 
     if (!resultLogger) {
-        resultLogger = new loggerModule.Logger(namespace, color, minLevelToLog);
+        resultLogger = new loggerModule.Logger(namespace, minLevelToLog);
         loggers[namespace] = resultLogger;
     }
 
     return resultLogger;
+}
+
+exports.GetFgColor = (colorString) => {
+    var color = enums.fgColors.get(colorString);
+    if (!color) {
+        internLogger.logWarning("Color " + colorString + " not found!");
+        return "";
+    }
+    return color;
+}
+exports.GetBgColor = (colorString) => {
+    var color = enums.bgColors.get(colorString);
+    if (!color) {
+        internLogger.logWarning("Color " + colorString + " not found!");
+        return "";
+    }
+    return color;
+}
+exports.GetSpecial = (specialString) => {
+    var color = enums.specials.get(specialString);
+    if (!color) {
+        internLogger.logWarning("Modificator " + specialString + " not found!");
+        return "";
+    }
+    return color;
 }
